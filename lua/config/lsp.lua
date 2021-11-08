1bf25fd11bbb14c52e43cc4538eaa97e6a3bcc67
Danny Bronzino <guiltypiggies@live.com>
2021-11-08 02:51:10 -0500

    clean lsp config

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -24,46 +24,16 @@ local custom_attach = function(client, bufnr)
 	buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
 	buf_set_keymap("n", "<C-]>", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
 	buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	buf_set_keymap(
-		"n",
-		"<C-k>",
-		"<cmd>lua vim.lsp.buf.signature_help()<CR>",
-		opts
-	)
-	buf_set_keymap(
-		"n",
-		"<space>wa",
-		"<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>",
-		opts
-	)
-	buf_set_keymap(
-		"n",
-		"<space>wr",
-		"<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>",
-		opts
-	)
-	buf_set_keymap(
-		"n",
-		"<space>wl",
-		"<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
-		opts
-	)
+	buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+	buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+	buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+	buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
 	buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
 	buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
 	buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
 	buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-	buf_set_keymap(
-		"n",
-		"<space>q",
-		"<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>",
-		opts
-	)
-	buf_set_keymap(
-		"n",
-		"<space>ca",
-		"<cmd>lua vim.lsp.buf.code_action()<CR>",
-		opts
-	)
+	buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+	buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
 
 	vim.cmd([[
     autocmd CursorHold <buffer> lua require('config.lsp').show_line_diagnostics()
@@ -71,20 +41,10 @@ local custom_attach = function(client, bufnr)
 
 	-- Set some key bindings conditional on server capabilities
 	if client.resolved_capabilities.document_formatting then
-		buf_set_keymap(
-			"n",
-			"<space>f",
-			"<cmd>lua vim.lsp.buf.formatting()<CR>",
-			opts
-		)
+		buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 	end
 	if client.resolved_capabilities.document_range_formatting then
-		buf_set_keymap(
-			"x",
-			"<space>f",
-			"<cmd>lua vim.lsp.buf.range_formatting()<CR><ESC>",
-			opts
-		)
+		buf_set_keymap("x", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR><ESC>", opts)
 	end
 
 	-- The blow command will highlight the current variable and its usages in the buffer.
@@ -105,41 +65,10 @@ local custom_attach = function(client, bufnr)
 	vim.notify(msg, "info", { title = "Nvim-config" })
 end
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(
-	vim.lsp.protocol.make_client_capabilities()
-)
+local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local lspconfig = require("lspconfig")
-
--- lspconfig.pylsp.setup({
-	-- on_attach = custom_attach,
-	-- capabilities = capabilities,
-	-- settings = {
-		-- pylsp = {
-			-- plugins = {
-				-- pylint = { enabled = true, executable = "pylint" },
-				-- pyflakes = { enabled = false },
-				-- pycodestyle = { enabled = false },
-				-- jedi_completion = { fuzzy = true },
-				-- pyls_isort = { enabled = true },
-				-- pylsp_mypy = { enabled = true },
-			-- },
-		-- },
-	-- },
-	-- flags = {
-		-- debounce_text_changes = 200,
-	-- },
--- })
-
--- lspconfig.clangd.setup({
-	-- on_attach = custom_attach,
-	-- capabilities = capabilities,
-	-- filetypes = { "c", "cpp", "cc" },
-	-- flags = {
-		-- debounce_text_changes = 500,
-	-- },
--- })
 
 -- set up texlab for latex
 lspconfig.texlab.setup({
@@ -199,32 +128,17 @@ require("lspconfig").sumneko_lua.setup({
 })
 
 -- Change diagnostic signs.
-vim.fn.sign_define(
-	"LspDiagnosticsSignError",
-	{ text = "✗", texthl = "LspDiagnosticsDefaultError" }
-)
-vim.fn.sign_define(
-	"LspDiagnosticsSignWarning",
-	{ text = "!", texthl = "LspDiagnosticsDefaultWarning" }
-)
-vim.fn.sign_define(
-	"LspDiagnosticsSignInformation",
-	{ text = "", texthl = "LspDiagnosticsDefaultInformation" }
-)
-vim.fn.sign_define(
-	"LspDiagnosticsSignHint",
-	{ text = "", texthl = "LspDiagnosticsDefaultHint" }
-)
+vim.fn.sign_define("LspDiagnosticsSignError", { text = "✗", texthl = "LspDiagnosticsDefaultError" })
+vim.fn.sign_define("LspDiagnosticsSignWarning", { text = "!", texthl = "LspDiagnosticsDefaultWarning" })
+vim.fn.sign_define("LspDiagnosticsSignInformation", { text = "", texthl = "LspDiagnosticsDefaultInformation" })
+vim.fn.sign_define("LspDiagnosticsSignHint", { text = "", texthl = "LspDiagnosticsDefaultHint" })
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-	vim.lsp.diagnostic.on_publish_diagnostics,
-	{
-		underline = false,
-		virtual_text = false,
-		signs = true,
-		update_in_insert = false,
-	}
-)
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+	underline = false,
+	virtual_text = false,
+	signs = true,
+	update_in_insert = false,
+})
 
 -- Refs: https://github.com/neovim/nvim-lspconfig/wiki/UI-customization#show-source-in-diagnostics
 -- vim.lsp.handlers["textDocument/publishDiagnostics"] =
