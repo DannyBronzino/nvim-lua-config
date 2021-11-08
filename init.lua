af35101de90c128f9e2c2517752e3940df1ba36d
Danny Bronzino <guiltypiggies@live.com>
2021-11-08 07:35:32 -0500

    create mappings

--- init.lua
+++ init.lua
@@ -1,12 +1,8 @@
 -- sources
 require("plugins")
 require("options")
+require("mappings")
 
 --Set colorscheme (order is important here)
 vim.o.termguicolors = true
-vim.cmd([[colorscheme OceanicNext]])
-
---Remap space as leader key
-vim.api.nvim_set_keymap("", "Space>", "<Nop>", { noremap = true, silent = true })
-vim.g.mapleader = ","
-vim.g.maplocalleader = ","
+vim.cmd([[colorscheme dracula]])
