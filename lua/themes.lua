5da31cbb78f04c4a5db3b17b517d68beb574adea
Danny Bronzino <guiltypiggies@live.com>
2021-12-12 17:35:29 -0500

    change highlight to be more readable

--- lua/themes.lua
+++ lua/themes.lua
@@ -4,3 +4,4 @@ vim.g.oceanic_next_terminal_italic = 1
 --Set colorscheme (order is important here)
 vim.o.termguicolors = true
 vim.cmd([[colorscheme doom-one]])
+vim.cmd([[hi WhichKeyValue guifg=Normal]])
