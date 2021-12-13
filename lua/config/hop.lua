5b9807c1e3bfaadc634f856f09c13c2989c0ee1e
Danny Bronzino <guiltypiggies@live.com>
2021-12-13 18:16:25 -0500

    modify hop

--- lua/config/hop.lua
+++ lua/config/hop.lua
@@ -1,18 +1,27 @@
-require('hop').setup({
-  case_insensitive = true,
+require("hop").setup({
+	case_insensitive = true,
+	char2_fallback_key = "<CR>",
 })
 
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-map('n', 'f', "<cmd>lua require'hop'.hint_char1()<cr>", opts)
+-- replace "t"
+map("n", "t", "<cmd>lua require'hop'.hint_char1()<cr>", opts)
+map("o", "t", "<cmd>lua require'hop'.hint_char1()<cr>", opts)
+map("v", "t", "<cmd>lua require'hop'.hint_char1()<cr>", opts)
 
-map('o', 'f', "<cmd>lua require'hop'.hint_char1()<cr>", opts)
+-- replace "f"
+map("n", "f", "<cmd>lua require'hop'.hint_char1({inclusive_jump = true})<cr>", opts)
+map("o", "f", "<cmd>lua require'hop'.hint_char1({inclusive_jump = true})<cr>", opts)
+map("v", "f", "<cmd>lua require'hop'.hint_char1({inclusive_jump = true})<cr>", opts)
 
-map('v', 'f', "<cmd>lua require'hop'.hint_char1()<cr>", opts)
+-- word jump
+map("n", "W", "<cmd>lua require'hop'.hint_words()<cr>", opts)
+map("o", "W", "<cmd>lua require'hop'.hint_words()<cr>", opts)
+map("v", "W", "<cmd>lua require'hop'.hint_words()<cr>", opts)
 
-map('n', 'W', "<cmd>lua require'hop'.hint_words()<cr>", opts)
-
-map('o', 'W', "<cmd>lua require'hop'.hint_words()<cr>", opts)
-
-map('v', 'W', "<cmd>lua require'hop'.hint_words()<cr>", opts)
+-- two character search
+map("n", "T", "<cmd>lua require'hop'.hint_char2()<cr>", opts)
+map("o", "T", "<cmd>lua require'hop'.hint_char2()<cr>", opts)
+map("v", "T", "<cmd>lua require'hop'.hint_char2()<cr>", opts)
