101ec78cb64c2af52595de24620c65ec64071957
Danny Bronzino <guiltypiggies@live.com>
2021-12-14 18:20:49 -0500

    change comment color to be readable

--- lua/themes.lua
+++ lua/themes.lua
@@ -1,7 +1,8 @@
 vim.g.oceanic_next_terminal_bold = 1
 vim.g.oceanic_next_terminal_italic = 1
 
---Set colorscheme (order is important here)
+-- Set colorscheme (order is important here)
 vim.o.termguicolors = true
 vim.cmd([[colorscheme doom-one]])
 vim.cmd([[hi WhichKeyValue guifg=Normal]])
+vim.cmd([[hi Comment guifg=darkgrey]])
