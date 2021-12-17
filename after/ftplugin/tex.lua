fa44a22641a6afc66af62e977aba99fdcc96fb25
Danny Bronzino <guiltypiggies@live.com>
2021-12-17 07:11:46 -0500

    remove abbreviations

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -5,15 +5,3 @@ vim.opt.textwidth = 0
 vim.opt.wrapmargin = 0
 vim.opt.foldlevel = 999
 vim.opt.colorcolumn = "0"
-
-local buf_map = vim.api.nvim_buf_set_keymap
-local opts = { noremap = true, silent = true }
-
--- Expansions for book I'm writing
-buf_map(0, "i", "\\tq", "\\tq{}<left>", opts)
-buf_map(0, "i", "...", "\\dots{}", opts)
-vim.cmd([[
-iabbrev <buffer> Qing \Qing{}
-iabbrev <buffer> Therese \Therese{}
-iabbrev <buffer> Lakshmi \Lakshmi{}
-]])
