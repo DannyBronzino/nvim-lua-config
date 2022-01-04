2efebc530b2104089f133205a99fc823a7f2cb92
Danny Bronzino <guiltypiggies@live.com>
2022-01-04 05:13:41 -0500

    change file name

--- lua/local_init.lua
+++ lua/local_init.lua
@@ -3,7 +3,7 @@ local scan = require("plenary.scandir")
 local files = scan.scan_dir(".", { depth = 0 })
 
 for _, v in pairs(files) do
-	if v == "./init.lua" then
-		vim.cmd(":luafile ./init.lua")
+	if v == "./linit.lua" then
+		vim.cmd(":luafile ./linit.lua")
 	end
 end
