8bde8aa39548bf4ccdfaf96072e79402855cdefa
Danny Bronzino <guiltypiggies@live.com>
2022-01-12 21:42:27 -0500

    clear

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,8 +1,3 @@
 -- Easier syntax
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
-
-map("n", "<c-K>", ":<C-U>lprevious<CR>zv", opts)
-map("n", "<c-J>", ":<C-U>lnext<CR>zv", opts)
-map("n", "<c-H>", ":<C-U>lfirst<CR>zv", opts)
-map("n", "<c-L>", ":<C-U>llast<CR>zv", opts)
