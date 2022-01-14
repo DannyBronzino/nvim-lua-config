0afc0d0a9e927a9d3a0640a2ce79198f13563ff7
Danny Bronzino <guiltypiggies@live.com>
2022-01-13 22:47:03 -0500

    switch to luasnip

--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -1,41 +1,52 @@
--- Setup nvim-cmp.
+require("luasnip.loaders.from_vscode").load()
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local luasnip = require("luasnip")
 local cmp = require("cmp")
 local lspkind = require("lspkind")
-
 cmp.setup({
 	snippet = {
 		expand = function(args)
-			-- For `ultisnips` user.
-			vim.fn["UltiSnips#Anon"](args.body)
+			luasnip.lsp_expand(args.body)
 		end,
 	},
 	mapping = {
-		["<Tab>"] = function(fallback)
+		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
 			else
 				fallback()
 			end
-		end,
-		["<S-Tab>"] = function(fallback)
+		end, { "i", "s" }),
+
+		["<S-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
 			else
 				fallback()
 			end
-		end,
+		end, { "i", "s" }),
+
 		["<Esc>"] = cmp.mapping.close(),
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
-		["<C-d>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-n>"] = cmp.mapping.scroll_docs(-4),
+		["<C-p>"] = cmp.mapping.scroll_docs(4),
 	},
 	sources = {
 		{ name = "nvim_lsp" }, -- For nvim-lsp
-		{ name = "ultisnips" }, -- For ultisnips user.
+		{ name = "luasnip" }, -- For luasnips user.
 		{ name = "nvim_lua" }, -- for nvim lua function
 		{ name = "path" }, -- for path completion
 		{ name = "buffer", keyword_length = 4 }, -- for buffer word completion
-		{ name = "emoji", insert = true }, -- emoji completion
 	},
 	completion = {
 		keyword_length = 1,
@@ -49,7 +60,7 @@ cmp.setup({
 			with_text = false,
 			menu = {
 				nvim_lsp = "[LSP]",
-				ultisnips = "[US]",
+				luasnips = "[LS]",
 				nvim_lua = "[Lua]",
 				path = "[Path]",
 				buffer = "[Buffer]",
