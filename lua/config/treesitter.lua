12c9b35b869b39509e5e417ae19463eae3868377
Danny Bronzino <guiltypiggies@live.com>
2022-01-15 17:30:27 -0500

    add bibtex to snsure_installed

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup({
-	ensure_installed = { "lua", "latex" },
+	ensure_installed = { "lua", "latex", "bibtex" },
 	highlight = {
 		enable = true,
 		custom_captures = {
