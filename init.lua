ccae9ebd0fb8e6220878998daf2c9d5cfa4708be
Danny Bronzino <guiltypiggies@live.com>
2022-01-16 10:24:26 -0500

    remove wilder

--- init.lua
+++ init.lua
@@ -11,9 +11,10 @@ let g:config_files = [
       \ 'autocommands.vim',
       \ 'mappings.vim',
       \ 'plugins.vim',
-      \ 'wilder.vim',
       \ ]
 
 for s:fname in g:config_files
   execute printf('source %s/vim/%s', stdpath('config'), s:fname)
 endfor]])
+
+-- \ 'wilder.vim',
