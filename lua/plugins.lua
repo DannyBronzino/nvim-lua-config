f464bf0766eaa80746ce104502dd92bb32721b64
Danny Bronzino <guiltypiggies@live.com>
2022-01-17 11:05:32 -0500

    change sequencing

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,9 +6,9 @@ local packer_install_dir = vim.g.package_home .. "/opt/packer.nvim"
 
 local plug_url_format = ""
 if vim.g.is_linux then
-  plug_url_format = "https://hub.fastgit.org/%s"
+	plug_url_format = "https://hub.fastgit.org/%s"
 else
-  plug_url_format = "https://github.com/%s"
+	plug_url_format = "https://github.com/%s"
 end
 
 local packer_repo = string.format(plug_url_format, "wbthomason/packer.nvim")
@@ -16,18 +16,24 @@ local install_cmd = string.format("10split |term git clone --depth=1 %s %s", pac
 
 -- Auto-install packer in case it hasn't been installed.
 if fn.glob(packer_install_dir) == "" then
-  vim.api.nvim_echo({ { "Installing packer.nvim", "Type" } }, true, {})
-  vim.cmd(install_cmd)
+	vim.api.nvim_echo({ { "Installing packer.nvim", "Type" } }, true, {})
+	vim.cmd(install_cmd)
 end
 
 -- Load packer.nvim
 vim.cmd("packadd packer.nvim")
-local util = require('packer.util')
+local util = require("packer.util")
 
 require("packer").startup({
 	function(use)
 		-- it is recommened to put impatient.nvim before any other plugins
-		use({ "lewis6991/impatient.nvim", config = [[require('impatient')]] })
+		use({
+			"lewis6991/impatient.nvim",
+			config = function()
+				require("impatient")
+				require("impatient").enable_profile()
+			end,
+		})
 
 		-- packer itself, can be optional
 		use({
@@ -229,7 +235,7 @@ require("packer").startup({
 		use({
 			"folke/which-key.nvim",
 			config = [[require("config.which-key")]],
-			event = "VimEnter",
+			event = "BufEnter",
 		})
 
 		-- file explorer
@@ -265,7 +271,7 @@ require("packer").startup({
 				{ "telescope-fzf-native.nvim" },
 			},
 			config = [[require("config.telescope")]],
-			event = "VimEnter",
+			event = "BufEnter",
 		})
 
 		use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
@@ -304,7 +310,7 @@ require("packer").startup({
 	},
 })
 
-local status, _ = pcall(require, 'packer_compiled')
+local status, _ = pcall(require, "packer_compiled")
 if not status then
-  vim.notify("Error requiring packer_compiled.lua: run PackerSync to fix!")
+	vim.notify("Error requiring packer_compiled.lua: run PackerSync to fix!")
 end
