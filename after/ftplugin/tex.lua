98a2b109697a8438c1036d645e331d3fedbdaa5c
Danny Bronzino <guiltypiggies@live.com>
2022-01-19 13:31:41 -0500

    set spell

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -5,4 +5,7 @@ vim.opt.textwidth = 0
 vim.opt.wrapmargin = 0
 vim.opt.foldlevel = 999
 vim.opt.colorcolumn = "0"
+-- the following are necessary fro cmp-spell
 vim.opt.spell = true
+vim.opt.spelllang = { "en_us" }
+vim.opt.spellsuggest = { "best", "5" }
