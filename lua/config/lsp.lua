daa0e257cf5c7d05fbbcd4f04e2845bf32e7db53
Danny Bronzino <guiltypiggies@live.com>
2022-01-21 15:39:55 -0500

    change sumneko, add lua-language-server to $PATH

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -100,7 +100,6 @@ table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
 require("lspconfig").sumneko_lua.setup({
-	cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
 	on_attach = custom_attach,
 	capabilities = capabilities,
 	settings = {
