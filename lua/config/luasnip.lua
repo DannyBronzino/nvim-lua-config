5c73cb333738e0a10b7c38da0b0c5a0996a5e2dc
Danny Bronzino <guiltypiggies@live.com>
2022-01-25 17:27:56 -0500

    remove personal snippets

--- lua/config/luasnip.lua
+++ lua/config/luasnip.lua
@@ -18,14 +18,5 @@ ls.config.set_config({ enable_autosnippets = true })
 ls.autosnippets = {
 	tex = {
 		s({ trig = "...", name = "Ellipsis", dscr = "\\ldots{}", wordTrig = false }, t("\\ldots{}")),
-
-		-- use csquotes package
-		s({ trig = "\\tq", name = "Text Quote", dscr = "\\textquote" }, { t("\\textquote{"), i(1), i(0), t("}") }),
-
-		-- add this to preamble: \newcommand{\chinesequote}[1]{$\lceil${#1}$\rfloor$}
-		s(
-			{ trig = "\\cq", name = "Chinese Quote", dscr = "\\chinesequote" },
-			{ t("\\chinesequote{"), i(1), i(0), t("}") }
-		),
 	},
 }
