e45c2dd8343cbaaf6217c41dc2e04cc8e3e62318
Danny Bronzino <guiltypiggies@live.com>
2022-01-25 14:11:47 -0500

    add tabstops

--- after/ftplugin/bib.lua
+++ after/ftplugin/bib.lua
@@ -2,3 +2,7 @@ vim.opt.wrap = false
 vim.opt.sidescroll = 5
 vim.opt.sidescrolloff = 2
 vim.opt.colorcolumn = "80"
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
