2ac7997cec3a0eaddd500fa2aa40202f38721a82
Danny Bronzino <guiltypiggies@live.com>
2022-01-25 17:33:50 -0500

    add more snippets

--- lua/config/luasnip.lua
+++ lua/config/luasnip.lua
@@ -18,5 +18,20 @@ ls.config.set_config({ enable_autosnippets = true })
 ls.autosnippets = {
 	tex = {
 		s({ trig = "...", name = "Ellipsis", dscr = "\\ldots{}", wordTrig = false }, t("\\ldots{}")),
+
+		-- use csquotes package
+		s({ trig = "\\tq", name = "Text Quote", dscr = "\\textquote" }, { t("\\textquote{"), i(1), i(0), t("}") }),
+
+		-- add this to preamble: \newcommand{\chinesequote}[1]{$\lceil${#1}$\rfloor$}
+		s(
+			{ trig = "\\cq", name = "Chinese Quote", dscr = "\\chinesequote" },
+			{ t("\\chinesequote{"), i(1), i(0), t("}") }
+		),
+
+		-- add this to preamble: \newcommand{\Qing}{Q\={\i}ng}
+		s({ trig = "Qing", name = "Qing", dscr = "\\Qing{}", wordTrig = false }, t("\\Qing{}")),
+
+		-- add this to preampble: add this to preampble: Th\'{e}r\`{e}se
+		s({ trig = "Therese", name = "Therese", dscr = "\\Therese{}", wordTrig = false }, t("\\Therese{}")),
 	},
 }
