f5b0a96c3460b063e55a5df301edf496e4abb371
Danny Bronzino <guiltypiggies@live.com>
2022-02-04 16:15:36 -0500

    put locals first

--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -1,11 +1,11 @@
+local luasnip = require("luasnip")
+local cmp = require("cmp")
+local lspkind = require("lspkind")
+
 local has_words_before = function()
 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
-
-local luasnip = require("luasnip")
-local cmp = require("cmp")
-local lspkind = require("lspkind")
 
 cmp.setup({
 	snippet = {
