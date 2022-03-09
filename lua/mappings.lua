675dad331eb437b74dc95cfce9ad46231ae8042e
Danny Bronzino <guiltypiggies@live.com>
2022-03-09 17:23:38 -0500

    move leader setting

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,3 +1,8 @@
 -- Easier syntax
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
+
+-- set leader to comma
+map("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
