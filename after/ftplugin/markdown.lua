641e7f92888c8d38c888956955354a4630bd9f80
Danny Bronzino <guiltypiggies@live.com>
2022-03-12 18:59:39 -0500

    change tabs to 4

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -2,3 +2,9 @@ vim.opt.wrap = true
 vim.opt.linebreak = true
 vim.opt.textwidth = 0
 vim.opt.foldlevel = 999
+
+-- tabs
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
