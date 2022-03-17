aac3faa6a492866ea8213a3bfb834151ad32761f
Danny Bronzino <guiltypiggies@live.com>
2022-03-16 23:36:42 -0400

    fix incorrect matchup setting

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -3,13 +3,13 @@ require("nvim-treesitter.configs").setup({
 	sync_install = true,
 	highlight = {
 		enable = true,
-		additional_vim_regex_highlighting = false,
+		additional_vim_regex_highlighting = true,
 	},
 	indent = {
 		enable = true,
 	},
 	matchup = {
-		enable = enable, -- mandatory, false will disable the whole extension
+		enable = true, -- mandatory, false will disable the whole extension
 		disable = {}, -- optional, list of language that will be disabled
 		-- [options]
 	},
