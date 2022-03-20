93686091a891c5ae1bf175d2f97e0dea7e3a7187
Danny Bronzino <guiltypiggies@live.com>
2022-03-20 12:39:30 -0400

    bugfix

--- lua/utils.lua
+++ lua/utils.lua
@@ -33,6 +33,10 @@ function M.move_line(Direction)
 
 	if Direction == "up" then
 		current_row = current_row - 2
+	elseif current_row == vim.api.nvim_buf_line_count(0) then -- prevents error if you are on the last line
+		do
+			return
+		end
 	end
 
 	-- prevents crash if you try to move the top line up
@@ -43,7 +47,7 @@ function M.move_line(Direction)
 	end
 
 	vim.api.nvim_del_current_line()
-	vim.api.nvim_buf_set_lines(0, current_row, current_row, true, { current_line })
+	vim.api.nvim_buf_set_lines(0, current_row, current_row, false, { current_line })
 	vim.api.nvim_win_set_cursor(0, { current_row + 1, 0 })
 end
 
@@ -52,12 +56,12 @@ function M.insert_blank_line(Direction)
 	local current_row = vim.api.nvim_win_get_cursor(0)[1]
 	local offset = 0
 
-  if Direction == "above" then
-    current_row = current_row - 1
+	if Direction == "above" then
+		current_row = current_row - 1
 		offset = 2 -- to make sure cursor stays on original line after insertion
-  end
+	end
 
-	vim.api.nvim_buf_set_lines(0, current_row, current_row, true, { "" })
+	vim.api.nvim_buf_set_lines(0, current_row, current_row, false, { "" })
 	vim.api.nvim_win_set_cursor(0, { current_row + offset, 0 })
 end
 
