828456cc49fb2810906c42d56f57005cbfc8c861
Danny Bronzino <guiltypiggies@live.com>
2022-03-23 08:44:20 -0400

    disable regex highlighting

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -3,7 +3,7 @@ require("nvim-treesitter.configs").setup({
 	sync_install = true,
 	highlight = {
 		enable = true,
-		additional_vim_regex_highlighting = true,
+		additional_vim_regex_highlighting = false,
 	},
 	indent = {
 		enable = true,
