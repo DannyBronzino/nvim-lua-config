5def880eb2c24a06ba6194792c456914445a217a
Danny Bronzino <guiltypiggies@live.com>
2022-03-23 09:04:32 -0400

    use lsp instead of tags

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,5 +1,12 @@
 local M = {}
 
+-- wrap telescope results
+vim.api.nvim_create_autocmd("User", {
+	pattern = "TelescopePreviewerLoaded",
+	command = "setlocal wrap",
+})
+
+-- use find_files if not in git project
 M.project_files = function()
 	local opts = {} -- define here if you want to define something
 	local ok = pcall(require("telescope.builtin").git_files, opts)
@@ -9,6 +16,9 @@ M.project_files = function()
 end
 
 local actions = require("telescope.actions")
+
+require("telescope").load_extension("fzf") -- FZF integration
+require("telescope").load_extension("packer") -- packer extension
 
 require("telescope").setup({
 	defaults = {
@@ -21,12 +31,12 @@ require("telescope").setup({
 		},
 		mappings = {
 			i = {
-				["<C-u>"] = false,
-				["<C-d>"] = false,
-				["<c-n>"] = actions.move_selection_next,
-				["<c-p>"] = actions.move_selection_previous,
-				["<tab>"] = actions.move_selection_previous,
-				["<S-tab>"] = actions.move_selection_next,
+				["<c-n>"] = actions.preview_scrolling_down,
+				["<c-p>"] = actions.preview_scrolling_up,
+				["<c-j>"] = actions.move_selection_next,
+				["<c-k>"] = actions.move_selection_previous,
+				["<tab>"] = actions.move_selection_next,
+				["<s-tab>"] = actions.move_selection_previous,
 				["<c-l>"] = actions.toggle_selection,
 			},
 			n = {
@@ -34,10 +44,51 @@ require("telescope").setup({
 			},
 		},
 	},
+	pickers = {
+		find_files = {
+			theme = "ivy",
+		},
+		git_files = {
+			theme = "ivy",
+		},
+		buffers = {
+			theme = "ivy",
+		},
+		current_buffer_fuzzy_find = {
+			theme = "ivy",
+		},
+		help_tags = {
+			theme = "ivy",
+		},
+		tags = {
+			theme = "ivy",
+		},
+		current_buffer_tags = {
+			theme = "ivy",
+		},
+		grep_string = {
+			theme = "cursor",
+		},
+		live_grep = {
+			theme = "ivy",
+		},
+		oldfiles = {
+			theme = "ivy",
+		},
+		lsp_document_symbols = {
+			theme = "ivy",
+		},
+		lsp_workspace_symbols = {
+			theme = "ivy",
+		},
+		lsp_dynamic_workspace_symbols = {
+			theme = "ivy",
+		},
+		registers = {
+			theme = "ivy",
+		},
+	},
 })
-
--- FZF integration
-require("telescope").load_extension("fzf")
 
 -- Add leader shortcuts
 vim.api.nvim_set_keymap(
@@ -67,7 +118,7 @@ vim.api.nvim_set_keymap(
 vim.api.nvim_set_keymap(
 	"n",
 	"<leader>ft",
-	[[<cmd>lua require("telescope.builtin").tags()<CR>]],
+	[[<cmd>lua require("telescope.builtin").lsp_workspace_symbols()<CR>]],
 	{ noremap = true, silent = true }
 )
 vim.api.nvim_set_keymap(
@@ -85,13 +136,28 @@ vim.api.nvim_set_keymap(
 vim.api.nvim_set_keymap(
 	"n",
 	"<leader>fo",
-	[[<cmd>lua require("telescope.builtin").tags{ only_current_buffer = true }<CR>]],
+	[[<cmd>lua require("telescope.builtin").lsp_document_symbols()<CR>]],
 	{ noremap = true, silent = true }
 )
 vim.api.nvim_set_keymap(
 	"n",
 	"<leader>?",
 	[[<cmd>lua require("telescope.builtin").oldfiles()<CR>]],
+	{ noremap = true, silent = true }
+)
+
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>fr",
+	[[<cmd>lua require("telescope.builtin").registers()<CR>]],
+	{ noremap = true, silent = true }
+)
+
+
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>fp",
+	[[<cmd>lua require("telescope").extensions.packer.packer(require("telescope.themes").get_ivy())<CR>]],
 	{ noremap = true, silent = true }
 )
 
