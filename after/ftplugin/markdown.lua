1f117a8bcc395beb0d483a63e6ae3cfbf47d6b42
Danny Bronzino <guiltypiggies@live.com>
2022-03-26 12:58:29 -0400

    add map for neoformat

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -8,3 +8,5 @@ vim.opt_local.tabstop = 4
 vim.opt_local.softtabstop = 4
 vim.opt_local.shiftwidth = 4
 vim.opt_local.expandtab = true
+
+vim.api.nvim_set_keymap("n", "<space>f", "<cmd>Neoformat<cr>", { noremap = true, silent = true }) -- format document
