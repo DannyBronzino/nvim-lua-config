f04d05489188f629e6e602a6e7d3fa8440b8e994
Danny Bronzino <guiltypiggies@live.com>
2022-03-27 23:35:07 -0400

    remove mappings for smart-pairs

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -94,12 +94,6 @@ require("packer").startup({
 											{ "'", "'", { ignore_pre = "\\a" } },
 										},
 									},
-									mapping = {
-										jump_left_in_any = "<m-{>",
-										jump_right_in_any = "<m-}>",
-										jump_left_out_any = "<m-[>",
-										jump_right_out_any = "<m-]>",
-									},
 								})
 							end,
 						},
