cecc105ec25e347c815867a37293594c526fd415
Danny Bronzino <guiltypiggies@live.com>
2022-03-27 23:19:22 -0400

    change ensure_installed

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup({
-	ensure_installed = "maintained",
+	ensure_installed = { "lua", "vim", "latex", "bibtex", "markdown" },
 	sync_install = true,
 	highlight = {
 		enable = true,
