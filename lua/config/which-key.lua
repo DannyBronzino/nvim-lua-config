6262481acf884eff9ce25709d075b7b901bda93a
Danny Bronzino <guiltypiggies@live.com>
2022-04-09 20:56:37 -0400

    disable spell, marks, registers

--- lua/config/which-key.lua
+++ lua/config/which-key.lua
@@ -2,10 +2,10 @@ vim.opt.timeoutlen = 1000 -- timeout before which-key appears
 
 require("which-key").setup({
 	plugins = {
-		marks = true, -- shows a list of your marks on ' and `
-		registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+		marks = false, -- shows a list of your marks on ' and `
+		registers = false, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
 		spelling = {
-			enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+			enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
 			suggestions = 9, -- how many suggestions should be shown in the list?
 		},
 		-- the presets plugin, adds help for a bunch of default keybindings in Neovim
