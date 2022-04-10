a6ef28aef348bef4d23b9173ef110a3d39e4f36a
Danny Bronzino <guiltypiggies@live.com>
2022-04-09 20:54:41 -0400

    map for luafile

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -2,3 +2,7 @@ vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.expandtab = true
+
+local map = require("utils").map
+
+map("n", "<F5>", "<cmd>luafile %<cr>") -- source file
