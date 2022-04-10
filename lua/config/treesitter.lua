ad21e38b3e0bffb398dc154462a98a416c6bf76b
Danny Bronzino <guiltypiggies@live.com>
2022-04-09 22:03:23 -0400

    turn on regex

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -3,7 +3,7 @@ require("nvim-treesitter.configs").setup({
 	sync_install = true,
 	highlight = {
 		enable = true,
-		additional_vim_regex_highlighting = false,
+		additional_vim_regex_highlighting = { "latex" },
 	},
 	indent = {
 		enable = true,
