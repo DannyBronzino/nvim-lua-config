e7bf52963208f51e36fe43de3392f608a9d9f040
Danny Bronzino <guiltypiggies@live.com>
2022-04-11 20:00:55 -0400

    snippet for memoir/verse

--- luasnippets/tex.lua
+++ luasnippets/tex.lua
@@ -11,8 +11,16 @@ local d = ls.dynamic_node
 local r = ls.restore_node
 local events = require("luasnip.util.events")
 local ai = require("luasnip.nodes.absolute_indexer")
+local utils = require("utils")
 
 ls.add_snippets("tex", {
+	s("trig", {
+		i(1),
+		f(function(args, snip, user_arg_1)
+			return args[1][1] .. user_arg_1
+		end, { 1 }, { user_args = { "Will be appended to text from i(0)" } }),
+		i(0),
+	}),
 	s({
 		trig = "autocite",
 		name = "Full Autocite",
@@ -23,6 +31,47 @@ ls.add_snippets("tex", {
 		i(2, "postnote"),
 		t("]{"),
 		i(3, "citemark"),
+		t("}"),
+		i(0),
+	}),
+	s({
+		trig = "\\verse",
+		name = "Verse Formatting",
+	}, {
+		t("\\settowidth{\\versewidth}["),
+		d(2, function(args)
+			results = args[1][1] -- first line
+
+			for count = 2, #args[1] do
+				-- remoes backslashes if present and trims whitespace
+				local old_line = utils.rtrim(utils.ltrim(string.gsub(results, [[\\]], [[]])))
+				local new_line = utils.rtrim(utils.ltrim(string.gsub(args[1][count], [[\\]], [[]])))
+
+				-- measures length and passes longer string
+				if #old_line < #new_line then
+					old_line = new_line
+				end
+				results = old_line -- the winner
+			end
+
+			-- snippet to return
+			local snip = sn(nil, { t(results) })
+			return snip
+		end, { 1 }),
+		t({
+			"]",
+			"",
+			"\\begin{verse}{\\versewidth}",
+			"",
+			"\t\\begin{altverse}",
+			"",
+			"\t\t",
+		}),
+		i(1, "VERSES"),
+		t({ "", "", "\t\\end{altverse}", "", "", "\\end{verse}", "", "\\citedpoemauthorright{" }),
+		i(3, "author"),
+		t("}{"),
+		i(4, "citemark"),
 		t("}"),
 		i(0),
 	}),
