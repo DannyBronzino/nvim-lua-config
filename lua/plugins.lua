cdbea5817191352307cd9a0492cef0ce2d35d69c
Danny Bronzino <guiltypiggies@live.com>
2022-04-21 15:55:20 -0400

    adjust matchup options

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -37,12 +37,17 @@ require("packer").startup({
 				{ -- highlights matching brackets
 					"andymass/vim-matchup",
 					setup = function() -- put the settings here for easy removal
+						-- turn features on or off
+						vim.g.matchup_matchparen_enabled = 1
+						vim.g.matchup_motion_enabled = 0
+						vim.g.matchup_text_obj_enabled = 0
+						vim.g.matchup_matchparen_hi_surround_always = 1 -- highlights surrounding matches always
+						vim.g.matchup_transmute_enabled = 1 -- easy change of tags
+
 						vim.g.matchup_matchparen_deferred = 1 -- improve performance
 						vim.g.matchup_matchparen_timeout = 100 -- improve performance
 						vim.g.matchup_matchparen_insert_timeout = 30 -- improve performance
 						vim.g.matchup_delim_noskips = 0 -- whether to enable matching inside comment or string
-						vim.g.matchup_matchparen_hi_surround_always = 1 -- highlights surrounding matches always
-						vim.g.matchup_transmute_enabled = 1 -- easy change of tags
 						vim.g.matchup_matchparen_offscreen = { method = "popup" } -- show offscreen match pair in popup window
 					end,
 				},
@@ -105,7 +110,7 @@ require("packer").startup({
 			requires = "tpope/vim-repeat",
 			config = function()
 				require("lightspeed").setup({
-					ignore_case = true,
+					ignore_case = true
 				})
 			end,
 			event = "BufEnter",
