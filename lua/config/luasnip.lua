2315c23a2b49a7a68b4eb827b6fd0fcd82c22c2f
Danny Bronzino <guiltypiggies@live.com>
2022-04-25 21:31:27 -0400

    remove extra code

--- lua/config/luasnip.lua
+++ lua/config/luasnip.lua
@@ -1,19 +1,10 @@
-require("luasnip.loaders.from_vscode").lazy_load()
-require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./my_snippets" } })
-require("luasnip.loaders.from_lua").lazy_load()
-
 local ls = require("luasnip")
-local s = ls.snippet
-local sn = ls.snippet_node
-local isn = ls.indent_snippet_node
-local t = ls.text_node
-local i = ls.insert_node
-local f = ls.function_node
-local c = ls.choice_node
-local d = ls.dynamic_node
-local r = ls.restore_node
-local events = require("luasnip.util.events")
-local ai = require("luasnip.nodes.absolute_indexer")
+
+require("luasnip.loaders.from_vscode").lazy_load()
+
+require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./my_snippets" } })
+
+require("luasnip.loaders.from_lua").lazy_load()
 
 ls.filetype_extend("bib", { "tex" })
 
