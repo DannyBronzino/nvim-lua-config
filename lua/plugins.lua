dcea47e0b6e2ab9667e9273e62495508c7e72bfe
Danny Bronzino <guiltypiggies@live.com>
2022-05-03 02:23:57 -0400

    consolidate treesitter config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -29,20 +29,6 @@ require("packer").startup({
 			requires = {
 				{ -- highlights matching brackets
 					"andymass/vim-matchup",
-					setup = function() -- put the settings here for easy removal
-						-- turn features on or off
-						vim.g.matchup_matchparen_enabled = 1
-						vim.g.matchup_motion_enabled = 0
-						vim.g.matchup_text_obj_enabled = 0
-						vim.g.matchup_matchparen_hi_surround_always = 1 -- highlights surrounding matches always
-						vim.g.matchup_transmute_enabled = 1 -- easy change of tags
-
-						vim.g.matchup_matchparen_deferred = 1 -- improve performance
-						vim.g.matchup_matchparen_timeout = 100 -- improve performance
-						vim.g.matchup_matchparen_insert_timeout = 30 -- improve performance
-						vim.g.matchup_delim_noskips = 0 -- whether to enable matching inside comment or string
-						vim.g.matchup_matchparen_offscreen = { method = "popup" } -- show offscreen match pair in popup window
-					end,
 				},
 			},
 			config = [[require("config.treesitter")]],
@@ -165,6 +151,7 @@ require("packer").startup({
 			"kyazdani42/nvim-tree.lua",
 			requires = { "kyazdani42/nvim-web-devicons" },
 			config = [[require('config.nvim-tree')]],
+			disable = true,
 		})
 
 		use({ -- indent markers
