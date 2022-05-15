29509621ea40c331e88d416693fcceee77963376
Danny Bronzino <guiltypiggies@live.com>
2022-05-15 07:37:37 -0400

    add sonokai

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -142,7 +142,16 @@ require("packer").startup({
 				vim.g.tokyonight_style = "night"
 				vim.g.tokyonight_transparent = true
 			end,
-			disable = true,
+		})
+
+		use({
+			"sainnhe/sonokai",
+			setup = function()
+				vim.g.sonokai_style = "andromeda"
+				vim.g.sonokai_enable_italic = 1
+				vim.g.sonokai_transparent_background = 1
+				vim.g.sonokai_better_performance = 1
+			end,
 		})
 
 		use({ -- indent markers
