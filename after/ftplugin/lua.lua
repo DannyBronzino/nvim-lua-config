08c702ef24eb5ee7c3cafd3e25e2d6853fe91c42
Danny Bronzino <guiltypiggies@live.com>
2022-05-29 14:36:01 -0400

    set tw=80

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -3,5 +3,6 @@ vim.opt_local.softtabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.expandtab = true
 vim.opt_local.foldlevel = 99
+vim.opt_local.textwidth = 80
 
 Map("n", "<F5>", "<cmd>luafile %<cr>") -- source file
