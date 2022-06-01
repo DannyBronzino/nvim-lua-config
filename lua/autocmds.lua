64269e7901ad6bf9ab67fc21d17d36eb2e74ab1d
Danny Bronzino <guiltypiggies@live.com>
2022-06-01 05:57:50 -0400

    format

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -2,55 +2,55 @@ local au = vim.api.nvim_create_autocmd
 
 -- change colorscheme
 au("Colorscheme", {
-  callback = function()
-    -- get current colorscheme
-    local function get_colorscheme()
-      local colorscheme = vim.api.nvim_exec([[colorscheme]], true)
-      if colorscheme == "dracula" then
-        return "dracula-nvim"
-      elseif colorscheme == "kanagawa" then
-        return "kanagawa"
-      elseif colorscheme == "nightfly" then
-        return "nightfly"
-      elseif colorscheme == "tokyonight" then
-        return "tokyonight"
-      else
-        return "auto"
-      end
-    end
+	callback = function()
+		-- get current colorscheme
+		local function get_colorscheme()
+			local colorscheme = vim.api.nvim_exec([[colorscheme]], true)
+			if colorscheme == "dracula" then
+				return "dracula-nvim"
+			elseif colorscheme == "kanagawa" then
+				return "kanagawa"
+			elseif colorscheme == "nightfly" then
+				return "nightfly"
+			elseif colorscheme == "tokyonight" then
+				return "tokyonight"
+			else
+				return "auto"
+			end
+		end
 
-    -- change lualine colorscheme
-    require("lualine").setup({
-      options = {
-        theme = get_colorscheme(),
-      },
-    })
-    vim.cmd([[hi BufferTabPageFill ctermbg=none guibg=none]])
-  end,
-  desc = "changes lualine colorscheme when nvim colorscheme changes"
+		-- change lualine colorscheme
+		require("lualine").setup({
+			options = {
+				theme = get_colorscheme(),
+			},
+		})
+		vim.cmd([[hi BufferTabPageFill ctermbg=none guibg=none]])
+	end,
+	desc = "changes lualine colorscheme when nvim colorscheme changes",
 })
 
 -- set numbers to relative when in Normal mode, absolute when in Insert
 local number_toggle = vim.api.nvim_create_augroup("number_toggle", { clear = true })
 
 au({ "BufEnter", "FocusGained", "InsertLeave", "WinEnter" }, {
-  callback = function()
-    if vim.opt.number:get() == true then
-      vim.opt.relativenumber = true
-    end
-  end,
-  group = number_toggle,
-  desc = "set numbers to relative when entering Normal mode"
+	callback = function()
+		if vim.opt.number:get() == true then
+			vim.opt.relativenumber = true
+		end
+	end,
+	group = number_toggle,
+	desc = "set numbers to relative when entering Normal mode",
 })
 
 au({ "BufLeave", "FocusLost", "InsertEnter", "WinLeave" }, {
-  callback = function()
-    if vim.opt.number:get() == true then
-      vim.opt.relativenumber = false
-    end
-  end,
-  group = number_toggle,
-  desc = "set numbers to absolute when entering Insert mode"
+	callback = function()
+		if vim.opt.number:get() == true then
+			vim.opt.relativenumber = false
+		end
+	end,
+	group = number_toggle,
+	desc = "set numbers to absolute when entering Insert mode",
 })
 
 -- local cursorline_toggle = vim.api.nvim_create_augroup("numbertoggle", { clear = true })
@@ -72,13 +72,13 @@ au({ "BufLeave", "FocusLost", "InsertEnter", "WinLeave" }, {
 -- })
 
 -- Highlight on yank
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
 })
 
 -- resume edit position
