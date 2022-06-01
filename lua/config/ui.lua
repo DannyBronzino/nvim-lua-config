d07d939e01a7ac3a3d0b308e961bfda937c1721a
Danny Bronzino <guiltypiggies@live.com>
2022-06-01 05:57:29 -0400

    reorder

--- lua/config/ui.lua
+++ lua/config/ui.lua
@@ -1,95 +1,95 @@
 local colors = require("kanagawa.colors").setup()
 
 local overrides = {
-  -- overide existing highlights
-  WhichKeyValue = { fg = colors.crystalBlue },
-  LineNr = { fg = colors.dragonBlue },
-  Comment = { fg = colors.springBlue },
-  Visual = { bg = colors.waveBlue2 },
-  IncSearch = { bg = colors.oniViolet },
-  MatchParen = { fg = colors.sakuraPink, style = "bold" }, -- for vim-matchup
-  Error = { fg = colors.peachRed, style = "bold" },
-  ErrorMsg = { fg = colors.peachRed, style = "bold" },
-  SpellBad = { fg = colors.peachRed, style = "bold" },
-  SpellCap = { fg = colors.peachRed, style = "bold" },
-  SpellRare = { fg = colors.peachRed, style = "bold" },
-  SpellLocal = { fg = colors.peachRed, style = "bold" },
-  String = { fg = colors.lightBlue },
-  TelescopePreviewLine = { link = "Cursorline" },
-  BufferTabPageFill = { bg = "none" },
+	-- overide existing highlights
+	WhichKeyValue = { fg = colors.crystalBlue },
+	LineNr = { fg = colors.dragonBlue },
+	Comment = { fg = colors.springBlue },
+	Visual = { bg = colors.waveBlue2 },
+	IncSearch = { bg = colors.oniViolet },
+	MatchParen = { fg = colors.sakuraPink, style = "bold" }, -- for vim-matchup
+	Error = { fg = colors.peachRed, style = "bold" },
+	ErrorMsg = { fg = colors.peachRed, style = "bold" },
+	SpellBad = { fg = colors.peachRed, style = "bold" },
+	SpellCap = { fg = colors.peachRed, style = "bold" },
+	SpellRare = { fg = colors.peachRed, style = "bold" },
+	SpellLocal = { fg = colors.peachRed, style = "bold" },
+	String = { fg = colors.lightBlue },
+	TelescopePreviewLine = { link = "Cursorline" },
+	BufferTabPageFill = { bg = "none" },
 }
 
 require("kanagawa").setup({
-  overrides = overrides,
-  transparent = true,
-  diminactive = true,
-  globalstatus = true,
+	overrides = overrides,
+	transparent = true,
+	diminactive = true,
+	globalstatus = true,
 })
-
-vim.cmd([[colorscheme kanagawa]])
 
 vim.g.sonokai_style = "andromeda"
 vim.g.sonokai_enable_italic = 1
 vim.g.sonokai_transparent_background = 1
 vim.g.sonokai_better_performance = 1
 
+vim.cmd([[colorscheme kanagawa]])
+
 -- displays spell indicator
 local function spell()
-  if vim.o.spell then
-    return string.format("SPELL")
-  end
+	if vim.o.spell then
+		return string.format("SPELL")
+	end
 
-  return ""
+	return ""
 end
 
 -- show word count
 local function get_words()
-  return tostring(vim.fn.wordcount().words .. " words")
+	return tostring(vim.fn.wordcount().words .. " words")
 end
 
 require("lualine").setup({
-  options = {
-    icons_enabled = true,
-    theme = "kanagawa",
-    section_separators = { left = "", right = "" },
-    component_separators = { left = "", right = "" },
-    disabled_filetypes = {},
-    always_divide_middle = true,
-    globalstatus = true,
-  },
-  sections = {
-    lualine_a = { "mode" },
-    lualine_b = {
-      "branch",
-      "diff",
-    },
-    lualine_c = { "filename" },
-    lualine_x = {
-      { spell, color = "WarningMsg" },
-      { get_words },
-      "filetype",
-    },
-    lualine_y = {
-      {
-        "diagnostics",
-        sources = { "nvim_diagnostic" },
-      },
-      "location",
-    },
-    lualine_z = { "progress" },
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = { "filename" },
-    lualine_x = { "location" },
-    lualine_y = {},
-    lualine_z = {},
-  },
-  tabline = {},
-  extensions = {
-    "quickfix",
-    "fugitive",
-    -- "nvim-tree",
-  },
+	options = {
+		icons_enabled = true,
+		theme = "kanagawa",
+		section_separators = { left = "", right = "" },
+		component_separators = { left = "", right = "" },
+		disabled_filetypes = {},
+		always_divide_middle = true,
+		globalstatus = true,
+	},
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = {
+			"branch",
+			"diff",
+		},
+		lualine_c = { "filename" },
+		lualine_x = {
+			{ spell, color = "WarningMsg" },
+			{ get_words },
+			"filetype",
+		},
+		lualine_y = {
+			{
+				"diagnostics",
+				sources = { "nvim_diagnostic" },
+			},
+			"location",
+		},
+		lualine_z = { "progress" },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { "filename" },
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+	extensions = {
+		"quickfix",
+		"fugitive",
+		-- "nvim-tree",
+	},
 })
