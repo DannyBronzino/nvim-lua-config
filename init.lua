428e613d45c7027106c841eb8be346fdde9b96df
Danny Bronzino <guiltypiggies@live.com>
2022-06-28 14:19:46 -0400

    add emmylua annotations and move global functions

--- init.lua
+++ init.lua
@@ -4,4 +4,16 @@ require("options")
 require("globals")
 require("mappings")
 require("autocmds")
-require("utils")
+
+-- global functions
+
+---pretty print table or function result
+---@param item function|table
+function Inspect(item)
+  print(vim.inspect(item))
+end
+
+-- join all non-blank lines
+function Join_lines()
+  vim.cmd([[%!fmt -999]])
+end
--- lua/utils.lua
+++ lua/utils.lua
@@ -1,8 +1,3 @@
----inspect something -@param item function|table
-function Inspect(item)
-  print(vim.inspect(item))
-end
-
 local M = {}
 
 ---easier vim.keymap.set syntax
@@ -30,31 +25,18 @@ function M.executable(name)
   return false
 end
 
--- can the directory be created?
-function M.may_create_dir()
-  local fpath = vim.fn.expand("<afile>")
-  local parent_dir = vim.fn.fnamemodify(fpath, ":p:h")
-  local res = vim.fn.isdirectory(parent_dir)
-
-  if res == 0 then
-    vim.fn.mkdir(parent_dir, "p")
-  end
-end
-
--- trim whitespace from beginning of string
+---trim whitespace from beginning of string
+---@param s string
+---@return string
 function M.ltrim(s)
   return s:match("^%s*(.*)")
 end
 
--- trim whitespace from end of string
+---trim whitespace from end of string
+---@param s string
+---@return string
 function M.rtrim(s)
   return s:match("^(.*%S)%s*$")
-end
-
--- join all non-blank lines
--- because I can never remember
-function M.join_lines()
-  vim.cmd([[%!fmt -999]])
 end
 
 return M
