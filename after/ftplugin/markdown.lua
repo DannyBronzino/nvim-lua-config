68b1bf7953a2020b7f9210cccaba851fd25e7973
Danny Bronzino <guiltypiggies@live.com>
2022-07-07 01:01:10 -0400

    remove neoformat

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -8,11 +8,3 @@ vim.opt_local.tabstop = 4
 vim.opt_local.softtabstop = 4
 vim.opt_local.shiftwidth = 4
 vim.opt_local.expandtab = true
-
-local map = require("utils").map
-
-map("n", "<space>f", "<cmd>Neoformat<cr>") -- format document
-
-map("n", "<leader>ft", function()
-  require("telescope.builtin").tags()
-end)
