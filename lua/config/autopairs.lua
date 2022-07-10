76be45e3a920772cb1f4ded93adb1c1cfd2a73c3
Danny Bronzino <guiltypiggies@live.com>
2022-07-10 00:34:08 -0400

    add check_ts

--- lua/config/autopairs.lua
+++ lua/config/autopairs.lua
@@ -2,6 +2,7 @@ local map = require("utils").map
 local npairs = require("nvim-autopairs")
 
 npairs.setup({
+  check_ts = true,
   disable_filetype = { "TelescopePrompt", "guihua", "guihua_rust", "clap_input" },
   map_bs = false,
   map_cr = false,
