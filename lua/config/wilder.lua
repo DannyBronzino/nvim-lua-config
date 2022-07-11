c4e049d93ccb6ec044194df5c722cac0703644c5
Danny Bronzino <guiltypiggies@live.com>
2022-07-10 23:09:31 -0400

    enhance wilder

--- lua/config/wilder.lua
+++ lua/config/wilder.lua
@@ -1,52 +1,82 @@
 local wilder = require("wilder")
 local colors = require("kanagawa.colors").setup()
-
 wilder.setup({ modes = { ":", "/", "?" } })
--- Disable Python remote plugin
-wilder.set_option("use_python_remote_plugin", 0)
 
 wilder.set_option("pipeline", {
   wilder.branch(
+    wilder.python_file_finder_pipeline({
+      file_command = { "fd", "-tf" },
+      dir_command = { "fd", "-td" },
+      filters = { "fuzzy_filter", "difflib_sorter" },
+    }),
+    wilder.substitute_pipeline({
+      pipeline = wilder.python_search_pipeline({
+        skip_cmdtype_check = 1,
+        pattern = wilder.python_fuzzy_pattern({
+          start_at_boundary = 0,
+        }),
+      }),
+    }),
     wilder.cmdline_pipeline({
       fuzzy = 2,
       fuzzy_filter = wilder.lua_fzy_filter(),
     }),
-    wilder.vim_search_pipeline()
+    {
+      wilder.check(function(ctx, x)
+        return x == ""
+      end),
+      wilder.history(),
+    },
+    wilder.python_search_pipeline({
+      pattern = wilder.python_fuzzy_pattern({
+        start_at_boundary = 0,
+      }),
+    })
   ),
+})
+
+local highlighters = {
+  wilder.pcre2_highlighter(),
+  wilder.lua_fzy_highlighter(),
+}
+
+local popupmenu_renderer = wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+  border = "rounded",
+  empty_message = wilder.popupmenu_empty_message_with_spinner(),
+  highlighter = highlighters,
+  left = {
+    " ",
+    wilder.popupmenu_devicons(),
+    wilder.popupmenu_buffer_flags({
+      flags = " a + ",
+      icons = { ["+"] = "", a = "", h = "" },
+    }),
+  },
+  right = {
+    " ",
+    -- wilder.popupmenu_scrollbar(),
+  },
+  highlights = {
+    accent = wilder.make_hl("WilderAccent", "Pmenu", { { a = 1 }, { a = 1 }, { foreground = colors.crystalBlue } }),
+  },
+  pumblend = 33,
+}))
+
+local wildmenu_renderer = wilder.wildmenu_renderer({
+  highlighter = highlighters,
+  separator = " · ",
+  left = { " ", wilder.wildmenu_spinner(), " " },
+  right = { " ", wilder.wildmenu_index() },
+  highlights = {
+    accent = wilder.make_hl("WilderAccent", "Pmenu", { { a = 1 }, { a = 1 }, { foreground = colors.crystalBlue } }),
+  },
 })
 
 wilder.set_option(
   "renderer",
   wilder.renderer_mux({
-    [":"] = wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
-      highlighter = wilder.lua_fzy_highlighter(),
-      left = {
-        " ",
-        wilder.popupmenu_devicons(),
-      },
-      highlights = {
-        border = "LineNr",
-        accent = wilder.make_hl("WilderAccent", "Pmenu", {
-          { a = 1 },
-          { a = 1 },
-          { foreground = colors.crystalBlue },
-        }),
-      },
-      border = "single",
-      pumblend = 20,
-    })),
-    ["/"] = wilder.wildmenu_renderer({
-      highlighter = wilder.lua_fzy_highlighter(),
-      separator = " · ",
-      left = { " ", wilder.wildmenu_spinner(), " " },
-      right = { " ", wilder.wildmenu_index() },
-      highlights = {
-        accent = wilder.make_hl("WilderAccent", "Pmenu", {
-          { a = 1 },
-          { a = 1 },
-          { foreground = colors.crystalBlue },
-        }),
-      },
-    }),
+    [":"] = popupmenu_renderer,
+    ["/"] = wildmenu_renderer,
+    substitute = wildmenu_renderer,
   })
 )
