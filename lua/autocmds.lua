503c926b666068b2751ecec5dbfbebe96e19924f
Danny Bronzino <guiltypiggies@live.com>
2022-07-16 13:11:20 -0400

    added remember.nvim

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -51,15 +51,3 @@ au("TextYankPost", {
   pattern = "*",
   desc = "highlights on yank",
 })
-
-au({ "BufReadPost" }, {
-  callback = function()
-    local row, column = unpack(vim.api.nvim_buf_get_mark(0, '"'))
-    local buf_line_count = vim.api.nvim_buf_line_count(0)
-
-    if row >= 1 and row <= buf_line_count then
-      vim.api.nvim_win_set_cursor(0, { row, column })
-    end
-  end,
-  desc = "resets cursor to last position",
-})
