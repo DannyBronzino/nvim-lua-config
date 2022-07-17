9de57ad5b11e55d35f1971d1cc8052c71d677c84
Danny Bronzino <guiltypiggies@live.com>
2022-07-17 17:57:35 -0400

    turn on regex highlighting

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -3,7 +3,7 @@ require("nvim-treesitter.configs").setup({
   sync_install = true,
   highlight = {
     enable = true,
-    additional_vim_regex_highlighting = {},
+    additional_vim_regex_highlighting = { "latex", "bibtex" },
   },
   indent = {
     enable = true,
