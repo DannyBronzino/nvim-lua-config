291673eb3b2fc7a75a87228f2d870a5c3f2f8628
Danny Bronzino <guiltypiggies@live.com>
2022-07-29 19:47:29 -0400

    fix resume edit

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -53,42 +53,55 @@ au({ "BufLeave", "FocusLost", "InsertEnter", "WinLeave" }, {
 -- desc = "highlights on yank",
 -- })
 
+local resume_edit = function()
+  local api = vim.api
+  -- this only works in the current buffer
+  local set_cursor = function(position)
+    return api.nvim_win_set_cursor(0, position)
+  end
+
+  -- get the mark
+  local last_insert_mark = api.nvim_buf_get_mark(0, "^")
+  -- get the total lines
+  local total_buf_lines = api.nvim_buf_line_count(0)
+
+  -- test to see if mark is outside of range
+  -- if not, move to mark
+  if pcall(set_cursor, last_insert_mark) then
+    set_cursor(last_insert_mark)
+
+    -- if mark is beyond last line, move cursor to last existing line
+  elseif last_insert_mark[1] > total_buf_lines then
+    set_cursor({ total_buf_lines, 0 })
+
+    -- if mark is past end of an existing line, then move cursor  to end of line
+  elseif pcall(set_cursor, { last_insert_mark[1], -1 }) then
+    set_cursor({ last_insert_mark[1], -1 })
+
+    -- if mark simply doesn't exist, e.g. first time opening file, place cursor on first character in buffer
+  else
+    set_cursor({ 1, 0 })
+  end
+
+  -- position line two rows from the top of the screen
+  -- see similar mapping in mappings.lua
+  -- if api.nvim_win_get_cursor(0)[1] > 3 then
+  -- api.nvim_feedkeys("zt2k2j", "n", true)
+  -- end
+end
+
 -- resume last insert position
-au("BufReadPost", {
+au("Filetype", {
   callback = function()
-    local api = vim.api
-    -- this only works in the current buffer
-    local set_cursor = function(position)
-      return api.nvim_win_set_cursor(0, position)
-    end
+    local is_in_table = require("utils").is_in_table
+    local blacklist =
+      { "guihua", "guihua_rust", "clap_input", "gitcommit", "gitrebase", "fzf", "notify", "packer", "help" }
+    local ft = vim.opt.filetype:get()
 
-    -- get the mark
-    local last_insert_mark = api.nvim_buf_get_mark(0, "^")
-    -- get the total lines
-    local total_buf_lines = api.nvim_buf_line_count(0)
-
-    -- test to see if mark is outside of range
-    -- if not, move to mark
-    if pcall(set_cursor, last_insert_mark) then
-      set_cursor(last_insert_mark)
-
-      -- if mark is beyond last line, move cursor to last existing line
-    elseif last_insert_mark[1] > total_buf_lines then
-      set_cursor({ total_buf_lines, 0 })
-
-      -- if mark is past end of an existing line, then move cursor  to end of line
-    elseif pcall(set_cursor, { last_insert_mark[1], -1 }) then
-      set_cursor({ last_insert_mark[1], -1 })
-
-      -- if mark simply doesn't exist, e.g. first time opening file, place cursor on first character in buffer
+    if is_in_table(blacklist, ft) then
+      return false
     else
-      set_cursor({ 1, 0 })
-    end
-
-    -- position line two rows from the top of the screen
-    -- see similar mapping in mappings.lua
-    if api.nvim_win_get_cursor(0)[1] > 3 then
-      api.nvim_feedkeys("zt2k2j", "n", true)
+      resume_edit()
     end
   end,
   desc = "places cursor at last insert position",
