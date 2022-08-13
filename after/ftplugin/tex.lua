848be36a296e1857868dff09dfb75c97c5c6c912
Danny Bronzino <guiltypiggies@live.com>
2022-08-13 17:32:01 -0400

    add buffer surround

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -4,3 +4,19 @@ vim.opt_local.textwidth = 0
 vim.opt_local.foldlevel = 99
 
 -- vim.opt_local.foldmethod = "syntax"
+require("nvim-surround").buffer_setup({
+  surrounds = {
+    ["c"] = {
+      add = function()
+        local cmd = require("nvim-surround.config").get_input("Command: ")
+        return { { "\\" .. cmd .. "{" }, { "}" } }
+      end,
+    },
+    ["e"] = {
+      add = function()
+        local env = require("nvim-surround.config").get_input("Environment: ")
+        return { { "\\begin{" .. env .. "}" }, { "\\end{" .. env .. "}" } }
+      end,
+    },
+  },
+})
