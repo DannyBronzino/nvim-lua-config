fb558a66d6c5ec52063d814be2b763eddb585a36
Danny Bronzino <guiltypiggies@live.com>
2022-08-13 17:31:46 -0400

    ditto

--- init.lua
+++ init.lua
@@ -7,6 +7,12 @@ require("autocmds")
 
 -- global functions
 
+---pretty print table or function result
+---@param item function|table
+function Inspect(item)
+  vim.pretty_print(item)
+end
+
 -- join all non-blank lines
 function Join_lines()
   vim.cmd([[%!fmt -999]])
