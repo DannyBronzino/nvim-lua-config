0f7589b510294ac280edcd8cfc0013712c4c1cd3
Danny Bronzino <guiltypiggies@live.com>
2022-09-05 05:08:18 -0400

    feat: map for fzf.help_tags

--- lua/config/fzf.lua
+++ lua/config/fzf.lua
@@ -658,16 +658,20 @@ end, { desc = "show document symbols with fzf" })
 
 map("n", "<leader>gw", function()
   require("fzf-lua").grep_cword()
-end, { desc = "grep for current word with fzf" })
+end, { desc = "grep for word under cursor with fzf" })
 
 map("n", "<leader>gp", function()
   require("fzf-lua").grep_project()
-end, { desc = "Live grep current project with fzf" })
+end, { desc = "live grep current project with fzf" })
 
 map("n", "<leader>gb", function()
   require("fzf-lua").grep_curbuf()
-end, { desc = "Live grep current buffer with fzf" })
+end, { desc = "live grep current buffer with fzf" })
 
 map("n", "<leader><space>", function()
   require("fzf-lua").oldfiles()
 end, { desc = "show list of old files with fzf" })
+
+map("n", "<leader>q", function()
+  require("fzf-lua").help_tags()
+end, { desc = "show help tags with fzf" })
