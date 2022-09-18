355f0925bd56417042cb5f9b39d67ef33fbf97f3
Danny Bronzino <guiltypiggies@live.com>
2022-09-17 22:04:56 -0400

    edit conf

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -87,8 +87,8 @@ cmp.setup.cmdline(":", {
   mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
     { name = "path" },
-  }, {
     { name = "cmdline" },
+    { name = "cmdline_history" },
   }),
 })
 
@@ -96,6 +96,7 @@ cmp.setup.cmdline("/", { -- search completion
   mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
     { name = "buffer" },
+    { name = "cmdline_history" },
   }),
 })
 
@@ -103,26 +104,27 @@ cmp.setup.cmdline("?", { -- reverse search completion
   mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
     { name = "buffer" },
+    { name = "cmdline_history" },
   }),
 })
 
 --  see https://github.com/hrsh7th/nvim-cmp/wiki/Menu-Appearance#how-to-add-visual-studio-code-dark-theme-colors-to-the-menu
-vim.cmd([[
-  highlight! link CmpItemMenu Comment
-  " gray
-  highlight! CmpItemAbbrDeprecated guibg=NONE gui=strikethrough guifg=#808080
-  " blue
-  highlight! CmpItemAbbrMatch guibg=NONE guifg=#569CD6
-  highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=#569CD6
-  " light blue
-  highlight! CmpItemKindVariable guibg=NONE guifg=#9CDCFE
-  highlight! CmpItemKindInterface guibg=NONE guifg=#9CDCFE
-  highlight! CmpItemKindText guibg=NONE guifg=#9CDCFE
-  " pink
-  highlight! CmpItemKindFunction guibg=NONE guifg=#C586C0
-  highlight! CmpItemKindMethod guibg=NONE guifg=#C586C0
-  " front
-  highlight! CmpItemKindKeyword guibg=NONE guifg=#D4D4D4
-  highlight! CmpItemKindProperty guibg=NONE guifg=#D4D4D4
-  highlight! CmpItemKindUnit guibg=NONE guifg=#D4D4D4
-]])
+-- vim.cmd([[
+-- highlight! link CmpItemMenu Comment
+-- " gray
+-- highlight! CmpItemAbbrDeprecated guibg=NONE gui=strikethrough guifg=#808080
+-- " blue
+-- highlight! CmpItemAbbrMatch guibg=NONE guifg=#569CD6
+-- highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=#569CD6
+-- " light blue
+-- highlight! CmpItemKindVariable guibg=NONE guifg=#9CDCFE
+-- highlight! CmpItemKindInterface guibg=NONE guifg=#9CDCFE
+-- highlight! CmpItemKindText guibg=NONE guifg=#9CDCFE
+-- " pink
+-- highlight! CmpItemKindFunction guibg=NONE guifg=#C586C0
+-- highlight! CmpItemKindMethod guibg=NONE guifg=#C586C0
+-- " front
+-- highlight! CmpItemKindKeyword guibg=NONE guifg=#D4D4D4
+-- highlight! CmpItemKindProperty guibg=NONE guifg=#D4D4D4
+-- highlight! CmpItemKindUnit guibg=NONE guifg=#D4D4D4
+-- ]])
