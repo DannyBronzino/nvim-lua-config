f6bfb47b037648d85c222c3881e3e52a0cb9627b
Danny Bronzino <guiltypiggies@live.com>
2022-09-26 15:13:24 -0400

    install sad, maybe

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -192,6 +192,22 @@ return require("packer").startup({
       after = "fzf-lua",
     })
 
+    use({
+      "ray-x/sad.nvim",
+      requires = "ray-x/guihua.lua",
+      config = function()
+        require("sad").setup({
+          diff = "delta", -- you can use `diff`, `diff-so-fancy`
+          ls_file = "fd", -- also git ls_file
+          exact = false, -- exact match
+          vsplit = false, -- split sad window the screen vertically, when set to number
+          -- it is a threadhold when window is larger than the threshold sad will split vertically,
+          height_ratio = 0.9, -- height ratio of sad window when split horizontally
+          width_ratio = 0.9, -- height ratio of sad window when split vertically
+        })
+      end,
+    })
+
     -- icons used by everything
     use({
       "kyazdani42/nvim-web-devicons",
