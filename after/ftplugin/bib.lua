394ece1418517223837d7371b12e585665729cb4
Danny Bronzino <guiltypiggies@live.com>
2022-10-02 23:41:32 -0400

    turn off conceal

--- after/ftplugin/bib.lua
+++ after/ftplugin/bib.lua
@@ -3,4 +3,4 @@ vim.opt_local.linebreak = true
 vim.opt_local.colorcolumn = "80"
 vim.opt_local.tabstop = 2
 
-vim.wo.conceallevel = 2
+-- vim.wo.conceallevel = 2
--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -3,4 +3,4 @@ vim.opt_local.linebreak = true
 vim.opt_local.textwidth = 0
 vim.opt_local.foldlevel = 99
 
-vim.wo.conceallevel = 2
+-- vim.wo.conceallevel = 2
