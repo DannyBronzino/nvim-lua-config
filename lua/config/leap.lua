2e4c75081dc8b493d9c56cbaff88c43292709242
Danny Bronzino <guiltypiggies@live.com>
2022-10-12 04:07:11 -0400

    remove unnecessary setup

--- lua/config/leap.lua
+++ lua/config/leap.lua
@@ -1,20 +1,3 @@
-require("leap").setup({
-  max_aot_targets = nil,
-  highlight_unlabeled = false,
-  case_sensitive = false,
-  -- Sets of characters that should match each other.
-  -- Obvious candidates are braces and quotes ('([{', ')]}', '`"\'').
-  equivalence_classes = { " \t\r\n" },
-  special_keys = {
-    repeat_search = "<enter>",
-    next_aot_match = "<enter>",
-    next_match = { ";", "<enter>" },
-    prev_match = { ",", "<tab>" },
-    next_group = "<space>",
-    prev_group = "<tab>",
-  },
-})
-
 local map = require("utils").map
 
 map("n", "gs", function()
