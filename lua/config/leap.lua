2ca76197e2028da0260e5b41ef928c6550458c5a
Danny Bronzino <guiltypiggies@live.com>
2022-10-20 16:54:33 -0400

    fix leap conf

--- lua/config/leap.lua
+++ lua/config/leap.lua
@@ -1,11 +1,63 @@
+local leap = require("leap")
 local map = require("utils").map
 
+map({ "x", "o" }, "z", function()
+  leap.leap({ offset = 1, inclusive_op = true })
+end, { desc = "leap forward to in visual and operator mode" })
+
+map({ "x", "o" }, "Z", function()
+  leap.leap({ backward = true })
+end, { desc = "leap backward to in visual and operator mode" })
+
 map("n", "gs", function()
-  require("leap").leap({
+  leap.leap({
     target_windows = vim.tbl_filter(function(win)
       return vim.api.nvim_win_get_config(win).focusable
     end, vim.api.nvim_tabpage_list_wins(0)),
   })
 end, { desc = "search in all windows (including the current one) on the tab page." })
 
-require("leap").set_default_keymaps()
+-- The following example showcases a custom action, using `multiselect`. We're
+-- executing a `normal!` command at each selected position (this could be even
+-- more useful if we'd pass in custom targets too).
+
+function paranormal(targets)
+  -- Get the :normal sequence to be executed.
+  local input = vim.fn.input("normal! ")
+  if #input < 1 then
+    return
+  end
+
+  local ns = vim.api.nvim_create_namespace("")
+
+  -- Set an extmark as an anchor for each target, so that we can also execute
+  -- commands that modify the positions of other targets (insert/change/delete).
+  for _, target in ipairs(targets) do
+    local line, col = unpack(target.pos)
+    id = vim.api.nvim_buf_set_extmark(0, ns, line - 1, col - 1, {})
+    target.extmark_id = id
+  end
+
+  -- Jump to each extmark (anchored to the "moving" targets), and execute the
+  -- command sequence.
+  for _, target in ipairs(targets) do
+    local id = target.extmark_id
+    local pos = vim.api.nvim_buf_get_extmark_by_id(0, ns, id, {})
+    vim.fn.cursor(pos[1] + 1, pos[2] + 1)
+    vim.cmd("normal! " .. input)
+  end
+
+  -- Clean up the extmarks.
+  vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
+end
+
+-- Usage:
+map("n", "<leader>s", function()
+  leap.leap({
+    target_windows = { vim.fn.win_getid() },
+    action = paranormal,
+    multiselect = true,
+  })
+end)
+
+leap.add_default_mappings()
