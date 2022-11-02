d3403a0861f3c243d6b8061b9c2501d0b05e4019
Danny Bronzino <guiltypiggies@live.com>
2022-11-02 03:32:41 -0400

    adjust noice

--- lua/config/noice.lua
+++ lua/config/noice.lua
@@ -57,7 +57,7 @@ require("noice").setup({
     },
     -- :Noice last
     last = {
-      view = "popup",
+      view = "split",
       opts = { enter = true, format = "details" },
       filter = {
         any = {
