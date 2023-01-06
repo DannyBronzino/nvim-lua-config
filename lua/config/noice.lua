7cee55c8405ea59f4643d9cfa1b2446664e5d798
Danny Bronzino <guiltypiggies@live.com>
2023-01-06 10:44:43 -0500

    remove @types

--- lua/config/noice.lua
+++ lua/config/noice.lua
@@ -3,7 +3,6 @@ require("noice").setup({
     enabled = true, -- enables the Noice cmdline UI
     view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
     opts = {}, -- global options for the cmdline. See section on views
-    ---@type table<string, CmdlineFormat>
     format = {
       -- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
       -- view: (default is cmdline view)
@@ -34,19 +33,16 @@ require("noice").setup({
     enabled = true, -- enables the Noice popupmenu UI
     ---@type 'nui'|'cmp'
     backend = "cmp", -- backend to use to show regular cmdline completions
-    ---@type NoicePopupmenuItemKind|false
     -- Icons for completion item kinds (see defaults at noice.config.icons.kinds)
     kind_icons = {}, -- set to `false` to disable icons
   },
   -- default options for require('noice').redirect
   -- see the section on Command Redirection
-  ---@type NoiceRouteConfig
   redirect = {
     view = "popup",
     filter = { event = "msg_show" },
   },
   -- You can add any custom commands below that will be available with `:Noice command`
-  ---@type table<string, NoiceCommand>
   commands = {
     history = {
       -- options for the message history that you get with `:Noice`
@@ -100,9 +96,7 @@ require("noice").setup({
       enabled = false,
       -- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
       -- See the section on formatting for more details on how to customize.
-      --- @type NoiceFormat|string
       format = "lsp_progress",
-      --- @type NoiceFormat|string
       format_done = "lsp_progress_done",
       throttle = 1000 / 30, -- frequency to update lsp progress message
       view = "mini",
@@ -118,7 +112,6 @@ require("noice").setup({
     hover = {
       enabled = true,
       view = nil, -- when nil, use defaults from documentation
-      ---@type NoiceViewOptions
       opts = {}, -- merged with defaults from documentation
     },
     signature = {
@@ -130,7 +123,6 @@ require("noice").setup({
         throttle = 50, -- Debounce lsp signature help request by 50ms
       },
       view = nil, -- when nil, use defaults from documentation
-      ---@type NoiceViewOptions
       opts = {}, -- merged with defaults from documentation
     },
     message = {
@@ -142,7 +134,6 @@ require("noice").setup({
     -- defaults for hover and signature help
     documentation = {
       view = "hover",
-      ---@type NoiceViewOptions
       opts = {
         lang = "markdown",
         replace = true,
@@ -175,7 +166,6 @@ require("noice").setup({
     -- add any filetypes here, that shouldn't trigger smart move.
     excluded_filetypes = { "cmp_menu", "cmp_docs", "notify" },
   },
-  ---@type NoicePresets
   presets = {
     -- you can enable a preset by setting it to true, or a table that will override the preset config
     -- you can also add custom presets that you can enable/disable with enabled=true
@@ -186,7 +176,6 @@ require("noice").setup({
     lsp_doc_border = false, -- add a border to hover docs and signature help
   },
   throttle = 1000 / 30, -- how frequently does Noice need to check for ui updates? This has no effect when in blocking mode.
-  ---@type NoiceConfigViews
   views = {
     cmdline_popup = {
       position = {
@@ -231,7 +220,6 @@ require("noice").setup({
       },
     },
   }, ---@see section on views
-  ---@type NoiceRouteConfig[]
   routes = {
     {
       filter = {
@@ -265,9 +253,7 @@ require("noice").setup({
       view = "mini",
     },
   }, --- @see section on routes
-  ---@type table<string, NoiceFilter>
   status = {}, --- @see section on statusline components
-  ---@type NoiceFormatOptions
   format = {}, --- @see section on formatting
 })
 
