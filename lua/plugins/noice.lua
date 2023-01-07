e5f5b489c6a1033076997eef7305b879cddf1ea1
Danny Bronzino <guiltypiggies@live.com>
2023-01-07 17:36:28 -0500

    adjust noice conf

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -44,8 +44,8 @@ return {
           -- This is a current Neovim limitation.
           enabled = true, -- enables the Noice messages UI
           view = "mini", -- default view for messages
-          view_error = "popup", -- view for errors
-          view_warn = "popup", -- view for warnings
+          view_error = "notify", -- view for errors
+          view_warn = "notify", -- view for warnings
           view_history = "popup", -- view for :messages
           view_search = false, -- view for search count messages. Set to `false` to disable
         },
