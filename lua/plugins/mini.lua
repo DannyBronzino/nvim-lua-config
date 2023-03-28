550d6d8360df9216107541cb8ec50c9ddec39d65
Danny Bronzino <guiltypiggies@live.com>
2023-03-28 10:44:48 -0400

    add mini.bracketed

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -5,12 +5,13 @@ return {
     lazy = false,
     version = false,
     config = function()
-      require("mini.ai").setup({}) -- adds more textobjects
-      require("mini.misc").setup({}) -- miscellaneous functions
+      require("mini.ai").setup() -- adds more textobjects
+      require("mini.misc").setup() -- miscellaneous functions
       require("mini.misc").setup_restore_cursor()
-      require("mini.move").setup({})
+      require("mini.move").setup()
+      require("mini.bracketed").setup()
       -- require("mini.animate").setup({}) -- animations
-      require("mini.trailspace").setup({}) -- identify and remove trailing spaces
+      require("mini.trailspace").setup() -- identify and remove trailing spaces
       vim.api.nvim_create_autocmd({ "BufWritePre" }, {
         pattern = "*",
         group = vim.api.nvim_create_augroup("MiniTrailSpace", { clear = true }),
