f6ec61eec93a6c29d422441d14fb28c1de1170e9
Danny Bronzino <guitlypiggies@live.com>
2023-07-21 21:00:12 -0400

    maybe switching from leap to flash

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,48 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = {
+    modes = {
+      -- use flash.nvim with f, F, t, T
+      char = {
+        jump_labels = true,
+      },
+    },
+  },
+  keys = {
+    {
+      "s",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash",
+    },
+    {
+      "S",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump({
+          pattern = vim.fn.expand("<cword>"),
+        })
+      end,
+      desc = "Flash with current word",
+    },
+    {
+      "r",
+      mode = "o",
+      function()
+        require("flash").remote()
+      end,
+      desc = "Remote Flash",
+    },
+    {
+      "<c-s>",
+      mode = { "c" },
+      function()
+        require("flash").toggle()
+      end,
+      desc = "Toggle Flash Search",
+    },
+  },
+}
