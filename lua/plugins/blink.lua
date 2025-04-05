f7b8cfa55648165e797f60ae40c9831af072601b
Danny Bronzino <guitlypiggies@live.com>
2025-04-05 06:28:05 -0400

    fixed nesting?

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -23,28 +23,28 @@ return {
     version = "*",
     opts = {
       snippets = { preset = "luasnip" },
-    },
-    completion = {
-      documentation = {
-        window = {
-          border = "single",
+      completion = {
+        documentation = {
+          window = {
+            border = "single",
+          },
         },
-      },
-      menu = {
-        border = "single",
-        draw = {
-          components = {
-            kind_icon = {
-              ellipsis = false,
-              text = function(ctx)
-                local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
-                return kind_icon
-              end,
-              -- Optionally, you may also use the highlights from mini.icons
-              highlight = function(ctx)
-                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-                return hl
-              end,
+        menu = {
+          border = "single",
+          draw = {
+            components = {
+              kind_icon = {
+                ellipsis = false,
+                text = function(ctx)
+                  local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+                  return kind_icon
+                end,
+                -- Optionally, you may also use the highlights from mini.icons
+                highlight = function(ctx)
+                  local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                  return hl
+                end,
+              },
             },
           },
         },
